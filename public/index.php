0ea81bf8336ca3ef2ffbe668c4221fc26eb38218
yse-yoo <youhei_yoshikawa@yse-c.net>
2026-05-15 15:35:24 +0900

    add routing file

--- public/index.php
+++ public/index.php
@@ -51,24 +51,6 @@ $admin = static function (callable $handler) use ($auth): callable {
     };
 };
 
-$router->get('/api/auth/me', [$auth, 'current']);
-$router->post('/api/auth/login', [$auth, 'login']);
-$router->post('/api/auth/logout', [$auth, 'logout']);
-
-$router->get('/api/categories', [$categories, 'index']);
-$router->post('/api/categories', $admin([$categories, 'store']));
-$router->put('/api/categories/{id}', $admin([$categories, 'update']));
-$router->delete('/api/categories/{id}', $admin([$categories, 'destroy']));
-
-$router->get('/api/products', [$products, 'index']);
-$router->get('/api/products/{id}', [$products, 'show']);
-$router->post('/api/product-images', $admin([$products, 'uploadImage']));
-$router->post('/api/products', $admin([$products, 'store']));
-$router->put('/api/products/{id}', $admin([$products, 'update']));
-$router->delete('/api/products/{id}', $admin([$products, 'destroy']));
-
-$router->get('/api/sales', $admin([$sales, 'index']));
-$router->get('/api/sales/{id}', $admin([$sales, 'show']));
-$router->post('/api/sales', [$sales, 'store']);
+require dirname(__DIR__) . '/routes/api.php';
 
 $router->dispatch($method, $path);
